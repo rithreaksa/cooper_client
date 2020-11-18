b45562a6b2f73c07292bbfe82fa668674f1719cc
psnoya <paulina.s.noya@gmail.com>
2020-11-18 21:53:15 +0100

    adding a new modules folder with cooperCalculator.js file, result calculator

--- src/components/DisplayCooperResult.jsx
+++ src/components/DisplayCooperResult.jsx
@@ -1,6 +1,25 @@
 import React from "react"
-const DisplayCooperResult = props => {
-    return <div></div>
-}
+import cooperCalculator from "../modules/cooperCalculator";
 
-export default DisplayCooperResult
+const DisplayCooperResult = ({ distance, gender, age }) => {
+    const calculate = () => {
+        return cooperCalculator(distance, gender, age);
+    };
+
+    const propsPassed = distance && age ? true : false;
+
+    return (
+        <>
+        {propsPassed && (
+            <>
+             <p id="cooper-message">
+              {age} y/o {gender} running {distance} meters.
+        </p>
+        <p id="cooper-result">Result: {calculate()}</p>
+        </>
+    )}
+    </>
+   );
+};
+
+export default DisplayCooperResult;
