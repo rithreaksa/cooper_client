1c04d2b6aab1053d55a91680aa2c7286b39f3ba0
Rithreaksa <wonderfulreaksa@gmail.com>
2020-11-19 10:49:35 +0100

    Create user can log in spec folder and write tests

---
+++ cypress/integration/userCanLogin.spec.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-undef */
+
+/// <reference types="Cypress" />
+
+describe("User authenticates", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("successfully with valid credentials", () => {
+    cy.get("#login").click();
+    cy.get("#login-form").within(() => {
+      cy.get("#email").type("user@mail.com");
+      cy.get("#password").type("password");
+      cy.get("button").contains("Submit").click();
+    });
+    cy.get("#message").should("contain", "Hi user@mail.com");
+  });
+
+  it("unsuccessfully with invalid credentials", () => {
+    cy.get("#login").click();
+    cy.get("#login-form").within(() => {
+      cy.get("#email").type("user@gmail.com");
+      cy.get("#password").type("wrongpassword");
+      cy.get('button').contains('Submit').click()
+    });
+    cy.get("#message").should("contain", "Invalid login credentials. Please try again.");
+  });
+});
