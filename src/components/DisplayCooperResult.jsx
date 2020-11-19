dfe978ddb11501fc2ce5fb7d06ed2a94a66aa088
Rithreaksa <wonderfulreaksa@gmail.com>
2020-11-19 10:52:58 +0100

    Add anxios and edit index.js

--- src/components/DisplayCooperResult.jsx
+++ src/components/DisplayCooperResult.jsx
@@ -1,25 +1,25 @@
-import React from "react"
+import React from "react";
 import cooperCalculator from "../modules/cooperCalculator";
 
 const DisplayCooperResult = ({ distance, gender, age }) => {
-    const calculate = () => {
-        return cooperCalculator(distance, gender, age);
-    };
+  const calculate = () => {
+    return cooperCalculator(distance, gender, age);
+  };
 
-    const propsPassed = distance && age ? true : false;
+  const propsPassed = distance && age ? true : false;
 
-    return (
+  return (
+    <>
+      {propsPassed && (
         <>
-        {propsPassed && (
-            <>
-             <p id="cooper-message">
-              {age} y/o {gender} running {distance} meters.
-        </p>
-        <p id="cooper-result">Result: {calculate()}</p>
+          <p id="cooper-message">
+            {age} y/o {gender} running {distance} meters.
+          </p>
+          <p id="cooper-result">Result: {calculate()}</p>
         </>
-    )}
+      )}
     </>
-   );
+  );
 };
 
 export default DisplayCooperResult;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import axios from "axios";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let apiUrl;
+if (process.env.NODE_ENV === "production") {
+  apiUrl = "https://rs-cooper-api.herokuapp.com/api/v1";
+} else {
+  apiUrl = "https://localhost:3000/api/v1";
+}
+axios.defaults.baseURL = apiUrl;
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
