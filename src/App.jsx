56dfdc9abbe9e38246a2e9ff1a45d4d5d4f0e3af
Rithreaksa <wonderfulreaksa@gmail.com>
2020-11-19 10:52:10 +0100

    Create loginForm and inputFields components and add those components into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,57 @@
+/* eslint-disable no-undef */
 import React, { Component } from "react";
+
 import DisplayCooperResult from "./components/DisplayCooperResult";
 import InputFields from "./components/InputFields";
+import LoginForm from "./components/LoginForm";
 
 class App extends Component {
   state = {
     distance: "",
     gender: "female",
-    age: ""
+    age: "",
+    renderLoginForm: false,
+    authenticated: false,
+    message: ""
   };
 
-  onChangeHandler = e => {
+  onChangeHandler = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
+  onLogin = async e => {
+    e.preventDefault();
+    const response = await authenticate(
+      e.target.email.value,
+      e.target.password.value
+    );
+    if (response.authenticated) {
+      this.setState({ authenticated: true });
+    } else {
+      this.setState({ message: response.message, renderLoginForm: false })
+    }
+  }
+
   render() {
+    const renderLogin = this.state.renderLoginForm ? (
+      <LoginForm submitFormHandler={this.onLogin} />
+    ) : (
+      <button
+        id="login"
+        onClick={() => this.setState({ renderLoginForm: true })}
+      >
+        Login
+      </button>
+    );
     return (
       <>
-      <InputFields onChangeHandler={this.onChangeHandler} />
-      <DisplayCooperResult
+        <InputFields onChangeHandler={this.onChangeHandler} />
+        <DisplayCooperResult
           distance={this.state.distance}
           gender={this.state.gender}
           age={this.state.age}
         />
+        {renderLogin}
       </>
     );
   }
